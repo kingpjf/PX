54f269092e92709529671cc05027eddb742f4902
kingpjf <r164@163.com>
2013-05-28 15:36:13 +0800

    初始化

---
+++ MiWeb/src/main/java/com/px/action/LoginAction.java
@@ -0,0 +1,17 @@
+package com.px.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jeff
+ * Date: 13-5-28
+ * Time: обнГ2:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoginAction extends ActionSupport{
+    @Override
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+}
---
+++ MiWeb/src/main/web/jsp/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jeff
+  Date: 13-5-28
+  Time: 下午2:59
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
